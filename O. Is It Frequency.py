93b75b5160e02cc9c11001207e1494e33f6b08d4
YoussefKhalaf <ykhalaf081@gmail.com>
2025-02-15 21:35:35 +0200

    p9

--- O. Is It Frequency.py
+++ O. Is It Frequency.py
@@ -4,6 +4,6 @@ dic={}
 for i in arr:
     dic[i]=dic.get(i,0)+1
 for i in range(1,m+1):
-    print(dic.get(i,0))
+    print(dic.get(i,0)) 
     
 # link => https://codeforces.com/group/5pUldkahAU/contest/510058/problem/O
