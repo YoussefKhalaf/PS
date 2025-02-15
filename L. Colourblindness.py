fcffcb91df11b201e7d89de8b9b7277952411640
YoussefKhalaf <ykhalaf081@gmail.com>
2025-02-15 04:29:36 +0200

    p6

---
+++ L. Colourblindness.py
@@ -0,0 +1,13 @@
+t=int(input())
+for _ in range(t):
+    no_columns=int(input())
+    list1=input().replace("G","B")
+    list2=input().replace("G","B")
+    if list1==list2:
+        print("YES")
+    else:
+        print("NO")
+    
+    
+    
+# link => https://codeforces.com/group/5pUldkahAU/contest/510058/problem/L
